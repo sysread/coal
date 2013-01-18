42551c7c2400df6989f1b92a2b9abce5205e2d5e
Jeff Ober <jeff.ober@coxinc.com>
2013-01-18 13:38:07 -0500

    Added basic module structure, renamed

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
+    name='coal',
+    description=('An event-based framework based on libevent2.'),
+    version='0.1',
+    author='Jeff Ober',
+    author_email='jeffober@gmail.com',
+    url='',
+
     cmdclass = {'build_ext': build_ext},
     ext_modules = [
         Extension("libevent", ["libevent.pyx"], libraries=['event']),
